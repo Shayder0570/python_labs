32f2486a6dc61c269851ae5feb56b83596c004f7
Shayder0570 <shayder13579@gmail.com>
2026-09-11 21:39:27 +0300

    ex6fast

--- src/lab1/06_och_and_zaoch.py
+++ src/lab1/06_och_and_zaoch.py
@@ -2,9 +2,7 @@ n=int(input())
 och=0
 zaoch=0
 for i in range(n):
-    fam,name,age,lab=str(input()).split()
+    fam,name,age,lab=input().split()
     if lab=="True":
         och+=1
-    elif lab=="False":
-        zaoch+=1
-print(f"{och} {zaoch}")
+print(f"{och} {n-och}")
