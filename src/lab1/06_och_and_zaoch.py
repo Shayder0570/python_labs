4b7a2b79deee0f43d93359a4da586c0e26ea32dd
Shayder0570 <shayder13579@gmail.com>
2026-09-16 13:18:43 +0300

    fix

--- src/lab1/06_och_and_zaoch.py
+++ src/lab1/06_och_and_zaoch.py
@@ -1,8 +1,8 @@
-n=int(input())
+n=int(input("in_1: "))
 och=0
 zaoch=0
 for i in range(n):
-    fam,name,age,lab=input().split()
+    fam,name,age,lab=input(f"in_{(i+2)}: ").split()
     if lab=="True":
         och+=1
 print(f"{och} {n-och}")
